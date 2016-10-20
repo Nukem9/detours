2a37394df4cadbf411915b1d9983c41462382c55
Nukem <Nukem@outlook.com>
2016-10-20 02:30:14 -0400

    Initial commit

---
+++ detours/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <vector>
+
+// Distorm v3.3 X86/X64
+#include "distorm/distorm.h"
+
+#include "AsmGen.h"
+#include "Reassembler.h"
+#include "Detours.h"
