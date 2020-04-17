32784f4b6e844b84c6a740906cf5ae9a66c4ba80
Nukem <Nukem@outlook.com>
2020-04-17 19:52:43 -0400

    Update

--- detours/Detours.cpp
+++ detours/Detours.cpp
@@ -6,7 +6,7 @@ namespace Detours
 
 	void SetGlobalOptions(uint32_t Options)
 	{
-		InterlockedExchange((volatile LONG *)&GlobalOptions, Options & OPT_MASK);
+		InterlockedExchange(&GlobalOptions, Options & OPT_MASK);
 	}
 
 	uint32_t GetGlobalOptions()
@@ -22,195 +22,156 @@ namespace Detours
 		return Address;
 	}
 
-	bool DetourAtomicCopy4X8(uint8_t *Target, uint8_t *Memory, sizeptr_t Length)
+	bool DetourCopyMemory(uintptr_t Target, uintptr_t Memory, size_t Length)
 	{
-		char buffer[8];
-
-		// Only 4/8byte sizes supported
-		if(Length > sizeof(buffer))
-			return false;
+		auto pvTarget = reinterpret_cast<void *>(Target);
+		auto pvMemory = reinterpret_cast<void *>(Memory);
 
 		DWORD dwOld = 0;
-		if (!VirtualProtect(Target, Length, PAGE_EXECUTE_READWRITE, &dwOld))
+		if (!VirtualProtect(pvTarget, Length, PAGE_EXECUTE_READWRITE, &dwOld))
 			return false;
 
-		if(Length <= 4)
-		{
-			// Copy the old data, rewrite with new
-			memcpy(&buffer, Target, 4);
-			memcpy(&buffer, Memory, Length);
-
-			// Write all 4 bytes at once
-			InterlockedExchange((volatile LONG *)Target, *(LONG *)&buffer);
-		}
-		else if(Length <= 8)
-		{
-			// Copy the old data, rewrite with new
-			memcpy(&buffer, Target, 8);
-			memcpy(&buffer, Memory, Length);
-
-			// Write all 8 bytes at once
-			_intrinInterlockedExchange64((volatile LONGLONG *)Target, *(LONGLONG *)&buffer);
-		}
+		memcpy(pvTarget, pvMemory, Length);
 
 		// Ignore if this fails, the memory was copied either way
-		VirtualProtect(Target, Length, dwOld, &dwOld);
+		VirtualProtect(pvTarget, Length, dwOld, &dwOld);
 		return true;
 	}
 
-	bool DetourCopyMemory(sizeptr_t Target, sizeptr_t Memory, sizeptr_t Length)
+	bool DetourFlushCache(uintptr_t Target, size_t Length)
 	{
-		DWORD dwOld = 0;
-		if (!VirtualProtect((void *)Target, Length, PAGE_EXECUTE_READWRITE, &dwOld))
-			return false;
-
-		memcpy((void *)Target, (void *)Memory, Length);
-
-		// Ignore if this fails, the memory was copied either way
-		VirtualProtect((void *)Target, Length, dwOld, &dwOld);
-		return true;
+		return FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<void *>(Target), Length) != FALSE;
 	}
 
-	bool DetourFlushCache(sizeptr_t Target, sizeptr_t Length)
-	{
-		return FlushInstructionCache(GetCurrentProcess(), (LPCVOID)Target, Length) != FALSE;
-	}
-
-	uint8_t *IATThunkHook(uint8_t *Module, PIMAGE_THUNK_DATA NameTable, PIMAGE_THUNK_DATA ImportTable, const char *API, uint8_t *Detour)
+	uintptr_t IATThunkHook(uintptr_t Module, PIMAGE_THUNK_DATA NameTable, PIMAGE_THUNK_DATA ImportTable, const char *API, uintptr_t Detour)
 	{
 		for (; NameTable->u1.Ordinal != 0; ++NameTable, ++ImportTable)
 		{
 			if (!IMAGE_SNAP_BY_ORDINAL(NameTable->u1.Ordinal))
 			{
-				auto importName = (PIMAGE_IMPORT_BY_NAME)(Module + NameTable->u1.ForwarderString);
-				auto funcName = (const char *)&importName->Name[0];
+				auto importName = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(Module + NameTable->u1.ForwarderString);
+				auto funcName = &importName->Name[0];
 
 				// If this is the function name we want, hook it
 				if (!strcmp(funcName, API))
 				{
-					uint8_t *original = (uint8_t *)ImportTable->u1.AddressOfData;
-					uint8_t *newPointer = Detour;
+					// Copy the pointer variable itself, not the function bytes
+					uintptr_t originalFunc = ImportTable->u1.AddressOfData;
+					uintptr_t newPointer = Detour;
 
-					// Swap the pointer atomically
-					if (!DetourAtomicCopy4X8((uint8_t *)&ImportTable->u1.AddressOfData, (uint8_t *)&newPointer, sizeof(ImportTable->u1.AddressOfData)))
-						return nullptr;
+					if (!DetourCopyMemory(reinterpret_cast<uintptr_t>(&ImportTable->u1.AddressOfData), reinterpret_cast<uintptr_t>(&newPointer), sizeof(ImportTable->u1.AddressOfData)))
+						return 0;
 
 					// Done
-					return original;
+					return originalFunc;
 				}
 			}
 		}
 
-		return nullptr;
+		return 0;
 	}
 
-	uint8_t *IATHook(uint8_t *Module, const char *ImportModule, const char *API, uint8_t *Detour)
+	uintptr_t IATHook(uintptr_t Module, const char *ImportModule, const char *API, uintptr_t Detour)
 	{
-		// Validate DOS Header
-		ULONG_PTR moduleBase = (ULONG_PTR)Module;
-		PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)moduleBase;
-
-		if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
-			return nullptr;
+		auto dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(Module);
+		auto ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(Module + dosHeader->e_lfanew);
 
 		// Validate PE Header and (64-bit|32-bit) module type
-		PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)(moduleBase + dosHeader->e_lfanew);
+		if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+			return 0;
 
 		if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
-			return nullptr;
+			return 0;
 
 		if (ntHeaders->OptionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR_MAGIC)
-			return nullptr;
+			return 0;
 
-		// Get the load configuration section which holds the security cookie address
+		// Get the load configuration section which holds the imports
 		auto dataDirectory = ntHeaders->OptionalHeader.DataDirectory;
-		DWORD sectionRVA = dataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
-		DWORD sectionSize = dataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size;
+		auto sectionRVA = dataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+		auto sectionSize = dataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size;
 
 		if (sectionRVA == 0 || sectionSize == 0)
-			return nullptr;
+			return 0;
 
 		// https://jpassing.com/2008/01/06/using-import-address-table-hooking-for-testing/
 		// https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/interception/interception_win.cc
 		//
 		// Iterate over each import descriptor
-		auto importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(moduleBase + sectionRVA);
+		auto importDescriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(Module + sectionRVA);
 
 		for (size_t i = 0; importDescriptor[i].Name != 0; i++)
 		{
-			PSTR dllName = (PSTR)(moduleBase + importDescriptor[i].Name);
+			auto dllName = reinterpret_cast<PSTR>(Module + importDescriptor[i].Name);
 
 			// Is this the specific module the user wants?
 			if (!_stricmp(dllName, ImportModule))
 			{
 				if (!importDescriptor[i].FirstThunk)
-					return false;
+					return 0;
 
-				auto nameTable = (PIMAGE_THUNK_DATA)(moduleBase + importDescriptor[i].OriginalFirstThunk);
-				auto importTable = (PIMAGE_THUNK_DATA)(moduleBase + importDescriptor[i].FirstThunk);
+				auto nameTable = reinterpret_cast<PIMAGE_THUNK_DATA>(Module + importDescriptor[i].OriginalFirstThunk);
+				auto importTable = reinterpret_cast<PIMAGE_THUNK_DATA>(Module + importDescriptor[i].FirstThunk);
 
-				uint8_t *original = IATThunkHook(Module, nameTable, importTable, API, Detour);
+				auto originalFunc = IATThunkHook(Module, nameTable, importTable, API, Detour);
 
-				if (!original)
+				if (!originalFunc)
 					continue;
 
-				return original;
+				return originalFunc;
 			}
 		}
 
 		// API or module name wasn't found
-		return nullptr;
+		return 0;
 	}
 
-	uint8_t *IATDelayedHook(uint8_t *Module, const char *ImportModule, const char *API, uint8_t *Detour)
+	uintptr_t IATDelayedHook(uintptr_t Module, const char *ImportModule, const char *API, uintptr_t Detour)
 	{
-		// Validate DOS Header
-		ULONG_PTR moduleBase = (ULONG_PTR)Module;
-		PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)moduleBase;
-
-		if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
-			return nullptr;
+		auto dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(Module);
+		auto ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(Module + dosHeader->e_lfanew);
 
 		// Validate PE Header and (64-bit|32-bit) module type
-		PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)(moduleBase + dosHeader->e_lfanew);
+		if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+			return 0;
 
 		if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
-			return nullptr;
+			return 0;
 
 		if (ntHeaders->OptionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR_MAGIC)
-			return nullptr;
+			return 0;
 
-		// Get the load configuration section which holds the security cookie address
+		// Get the load configuration section which holds the imports
 		auto dataDirectory = ntHeaders->OptionalHeader.DataDirectory;
-		DWORD sectionRVA = dataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].VirtualAddress;
-		DWORD sectionSize = dataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].Size;
+		auto sectionRVA = dataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].VirtualAddress;
+		auto sectionSize = dataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT].Size;
 
 		if (sectionRVA == 0 || sectionSize == 0)
-			return nullptr;
+			return 0;
 
 		// Iterate over each delayed import descriptor
-		auto importDescriptor = (PIMAGE_DELAYLOAD_DESCRIPTOR)(moduleBase + sectionRVA);
+		auto importDescriptor = reinterpret_cast<PIMAGE_DELAYLOAD_DESCRIPTOR>(Module + sectionRVA);
 
 		for (size_t i = 0; importDescriptor[i].DllNameRVA != 0; i++)
 		{
-			PSTR dllName = (PSTR)(moduleBase + importDescriptor[i].DllNameRVA);
+			auto dllName = reinterpret_cast<PSTR>(Module + importDescriptor[i].DllNameRVA);
 
 			// Is this the specific module the user wants?
 			if (!_stricmp(dllName, ImportModule))
 			{
-				auto nameTable = (PIMAGE_THUNK_DATA)(moduleBase + importDescriptor[i].ImportNameTableRVA);
-				auto importTable = (PIMAGE_THUNK_DATA)(moduleBase + importDescriptor[i].ImportAddressTableRVA);
+				auto nameTable = reinterpret_cast<PIMAGE_THUNK_DATA>(Module + importDescriptor[i].ImportNameTableRVA);
+				auto importTable = reinterpret_cast<PIMAGE_THUNK_DATA>(Module + importDescriptor[i].ImportAddressTableRVA);
 
-				uint8_t *original = IATThunkHook(Module, nameTable, importTable, API, Detour);
+				auto originalFunc = IATThunkHook(Module, nameTable, importTable, API, Detour);
 
-				if (!original)
+				if (!originalFunc)
 					continue;
 
-				return original;
+				return originalFunc;
 			}
 		}
 
 		// API or module name wasn't found
-		return nullptr;
+		return 0;
 	}
 }
--- detours/Detours64.cpp
+++ detours/Detours64.cpp
@@ -16,16 +16,17 @@ namespace Detours
 		#define PUSH_RET_LENGTH_64	0x0E	// push <low 32 addr>; mov [rsp+4h], <hi 32 addr>; ret;
 		#define RAX_JUMP_64			0x0C	// mov rax, <addr>; jmp rax;
 		#define REL32_JUMP_64		0x05	// jmp <address within +/- 2GB>
+		#define REL32_CALL_64		0x05	// call <address within +/- 2GB>
 
-		#define ALIGN_64(x)			DetourAlignAddress((uint64_t)(x), 0x10);
+		#define ALIGN_64(x)			DetourAlignAddress((uint64_t)(x), 16);
 		#define BREAK_ON_ERROR()	{ if (GetGlobalOptions() & OPT_BREAK_ON_FAIL) __debugbreak(); }
 
-		uint8_t *DetourFunction(uint8_t *Target, uint8_t *Detour, X64Option Options)
+		uintptr_t DetourFunction(uintptr_t Target, uintptr_t Detour, X64Option Options)
 		{
 			if (!Target || !Detour)
 			{
 				BREAK_ON_ERROR();
-				return nullptr;
+				return 0;
 			}
 
 			// Init decoder exactly once
@@ -37,14 +38,14 @@ namespace Detours
 			if (!decoderInit)
 			{
 				BREAK_ON_ERROR();
-				return nullptr;
+				return 0;
 			}
 
 			// Decode the actual assembly
 			uint32_t neededSize = DetourGetHookLength(Options);
 			uint32_t totalInstrSize = 0;
 
-			for (int len = 0; len != -1; len = DetoursGetNextInstructionLength((void *)(Target + totalInstrSize)))
+			for (int len = 0; len != -1; len = DetoursGetNextInstructionLength(reinterpret_cast<void *>(Target + totalInstrSize)))
 			{
 				totalInstrSize += len;
 
@@ -56,7 +57,7 @@ namespace Detours
 			if (neededSize == 0 || totalInstrSize < neededSize)
 			{
 				BREAK_ON_ERROR();
-				return nullptr;
+				return 0;
 			}
 
 			// Allocate the trampoline data
@@ -70,51 +71,51 @@ namespace Detours
 			uint8_t *jumpTrampolinePtr = nullptr;
 			
 			// NOTE: If we're doing a REL32 hook, we need to do some magic
-			if (Options == X64Option::USE_REL32_JUMP)
+			if (Options == X64Option::USE_REL32_JUMP || Options == X64Option::USE_REL32_CALL)
 			{
 				// Find a region within 2GB (subtract some for tolerance)
-				sizeptr_t maxDelta	= (1ull * 1024 * 1024 * 1024) - 4096;
+				const uintptr_t maxDelta = (1ull * 1024 * 1024 * 1024) - 4096;
 
-				sizeptr_t start = ((sizeptr_t)Target - maxDelta);
-				sizeptr_t end	= ((sizeptr_t)Target + maxDelta);
+				uintptr_t start = Target - maxDelta;
+				uintptr_t end = Target + maxDelta;
 
 				while (start < end)
 				{
-					MEMORY_BASIC_INFORMATION memInfo;
-					if (VirtualQuery((LPVOID)start, &memInfo, sizeof(memInfo)) == 0)
+					MEMORY_BASIC_INFORMATION memInfo = {};
+					if (VirtualQuery(reinterpret_cast<void *>(start), &memInfo, sizeof(memInfo)) == 0)
 						break;
 
 					if (memInfo.State == MEM_FREE && memInfo.RegionSize >= allocSize)
 					{
-						jumpTrampolinePtr = (uint8_t *)VirtualAlloc(memInfo.BaseAddress, allocSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+						jumpTrampolinePtr = static_cast<uint8_t *>(VirtualAlloc(memInfo.BaseAddress, allocSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE));
 
 						if (jumpTrampolinePtr)
 							break;
 					}
 
-					start = (sizeptr_t)memInfo.BaseAddress + 4096 + 1;
+					start = reinterpret_cast<uintptr_t>(memInfo.BaseAddress) + 4096 + 1;
 				}
 			}
 			else
 			{
 				// Can be allocated anywhere
-				jumpTrampolinePtr = (uint8_t *)VirtualAlloc(nullptr, allocSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+				jumpTrampolinePtr = static_cast<uint8_t *>(VirtualAlloc(nullptr, allocSize, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE));
 			}
 
 			if (!jumpTrampolinePtr)
 			{
 				BREAK_ON_ERROR();
-				return nullptr;
+				return 0;
 			}
 
 			// Fill out the header
-			JumpTrampolineHeader *header = (JumpTrampolineHeader *)jumpTrampolinePtr;
+			auto header					= reinterpret_cast<JumpTrampolineHeader *>(jumpTrampolinePtr);
 
 			header->Magic				= HEADER_MAGIC;
 			header->Random				= GetCurrentProcessId() + GetCurrentThreadId() + totalInstrSize;
 
-			header->CodeOffset			= (sizeptr_t)Target;
-			header->DetourOffset		= (sizeptr_t)Detour;
+			header->CodeOffset			= Target;
+			header->DetourOffset		= Detour;
 
 			header->InstructionLength	= totalInstrSize;
 			header->InstructionOffset	= ALIGN_64(jumpTrampolinePtr + sizeof(JumpTrampolineHeader));
@@ -128,14 +129,25 @@ namespace Detours
 			// Write the assembly in the allocation block
 			DetourWriteStub(header);
 
-			bool result = true;
+			// Write padding nops if the hook splices the middle of an instruction
+			if ((GetGlobalOptions() & OPT_DO_NOT_PAD_NOPS) == 0)
+			{
+				std::vector<uint8_t> nops;
+				
+				for (uint32_t i = 0; i < header->InstructionLength; i++)
+					nops.push_back(0x90);
+
+				DetourCopyMemory(header->InstructionOffset, reinterpret_cast<uintptr_t>(nops.data()), nops.size());
+			}
+
+			bool result = false;
 
 			switch (Options)
 			{
 			case X64Option::USE_PUSH_RET:	result = DetourWritePushRet(header);	break;
 			case X64Option::USE_RAX_JUMP:	result = DetourWriteRaxJump(header);	break;
 			case X64Option::USE_REL32_JUMP:	result = DetourWriteRel32Jump(header);	break;
-			default:						result = false;							break;
+			case X64Option::USE_REL32_CALL:	result = DetourWriteRel32Call(header);	break;
 			}
 
 			if (!result)
@@ -143,21 +155,21 @@ namespace Detours
 				VirtualFree(jumpTrampolinePtr, 0, MEM_RELEASE);
 
 				BREAK_ON_ERROR();
-				return nullptr;
+				return 0;
 			}
 
 			// Force flush any possible CPU cache
-			DetourFlushCache((sizeptr_t)Target, totalInstrSize);
-			DetourFlushCache((sizeptr_t)jumpTrampolinePtr, allocSize);
+			DetourFlushCache(Target, totalInstrSize);
+			DetourFlushCache(reinterpret_cast<uintptr_t>(jumpTrampolinePtr), allocSize);
 
 			// Set read/execution on the page
 			DWORD dwOld = 0;
 			VirtualProtect(jumpTrampolinePtr, allocSize, PAGE_EXECUTE_READ, &dwOld);
 
-			return (uint8_t *)header->InstructionOffset;
+			return header->InstructionOffset;
 		}
 
-		bool DetourRemove(uint8_t *Trampoline)
+		bool DetourRemove(uintptr_t Trampoline)
 		{
 			if (!Trampoline)
 			{
@@ -165,7 +177,7 @@ namespace Detours
 				return false;
 			}
 
-			JumpTrampolineHeader *header = (JumpTrampolineHeader *)(Trampoline - sizeof(JumpTrampolineHeader));
+			auto header = reinterpret_cast<JumpTrampolineHeader *>(Trampoline - sizeof(JumpTrampolineHeader));
 
 			if (header->Magic != HEADER_MAGIC)
 			{
@@ -186,45 +198,46 @@ namespace Detours
 			return true;
 		}
 
-		uint8_t *DetourVTable(uint8_t *Target, uint8_t *Detour, uint32_t TableIndex)
+		uintptr_t DetourVTable(uintptr_t Target, uintptr_t Detour, uint32_t TableIndex)
 		{
 			// Each function is stored in an array
-			uint8_t *virtualPointer = (Target + (TableIndex * sizeof(ULONG64)));
+			auto virtualPointer = reinterpret_cast<void *>(Target + (TableIndex * sizeof(void *)));
 
 			DWORD dwOld = 0;
-			if (!VirtualProtect(virtualPointer, sizeof(ULONG64), PAGE_EXECUTE_READWRITE, &dwOld))
-				return nullptr;
+			if (!VirtualProtect(virtualPointer, sizeof(void *), PAGE_EXECUTE_READWRITE, &dwOld))
+				return 0;
 
-			uint8_t *original = (uint8_t *)InterlockedExchange64((volatile LONG64 *)virtualPointer, (LONG64)Detour);
+			auto original = InterlockedExchangePointer(reinterpret_cast<void **>(virtualPointer), reinterpret_cast<void *>(Detour));
 
-			VirtualProtect(virtualPointer, sizeof(LONG64), dwOld, &dwOld);
-			return original;
+			VirtualProtect(virtualPointer, sizeof(void *), dwOld, &dwOld);
+			return reinterpret_cast<uintptr_t>(original);
 		}
 
-		bool VTableRemove(uint8_t *Target, uint8_t *Function, uint32_t TableIndex)
+		bool VTableRemove(uintptr_t Target, uintptr_t Function, uint32_t TableIndex)
 		{
 			// Reverse VTable detour
-			return DetourVTable(Target, Function, TableIndex) != nullptr;
+			return DetourVTable(Target, Function, TableIndex) != 0;
 		}
 
 		void DetourWriteStub(JumpTrampolineHeader *Header)
 		{
-			auto writeJmp = [](sizeptr_t Base, sizeptr_t Destination)
+			auto writeJmp = [](uintptr_t Base, uintptr_t Destination)
 			{
 				uint8_t buffer[14];
 
 				// 'jmp qword ptr [rip + 0x6]' - unaligned
 				buffer[0] = 0xFF;
 				buffer[1] = 0x25;
-				*(uint32_t *)&buffer[2] = 0x0;
-				*(uint64_t *)&buffer[6] = Destination;
+				*reinterpret_cast<uint32_t *>(&buffer[2]) = 0;
+				*reinterpret_cast<uint64_t *>(&buffer[6]) = Destination;
 
-				return DetourCopyMemory(Base, (sizeptr_t)&buffer, sizeof(buffer));
+				return DetourCopyMemory(Base, (uintptr_t)&buffer, sizeof(buffer));
 			};
 
 			// Jump to hooked function (Backed up instructions) [UserFunction -> OldInstructions -> THIS -> HookedFunction]
-			uintptr_t unhookStart	= (Header->CodeOffset + Header->InstructionLength);
-			uintptr_t instr_ptr		= (Header->InstructionOffset + Header->InstructionLength);
+			auto unhookStart = Header->CodeOffset + Header->InstructionLength;
+			auto instr_ptr = Header->InstructionOffset + Header->InstructionLength;
+
 			writeJmp(instr_ptr, unhookStart);
 
 			// Jump to user function (Write the trampoline) [HookedFunction -> THIS -> UserFunction]
@@ -242,15 +255,15 @@ namespace Detours
 			// ret
 			//
 			buffer[0] = 0x68;
-			*(uint32_t *)&buffer[1] = (uint32_t)(Header->TrampolineOffset & 0xFFFFFFFF);
+			*reinterpret_cast<uint32_t *>(&buffer[1]) = static_cast<uint32_t>(Header->TrampolineOffset & 0xFFFFFFFF);
 			buffer[5] = 0xC7;
 			buffer[6] = 0x44;
 			buffer[7] = 0x24;
 			buffer[8] = 0x04;
-			*(uint32_t *)&buffer[9] = (uint32_t)(Header->TrampolineOffset >> 32);
+			*reinterpret_cast<uint32_t *>(&buffer[9]) = static_cast<uint32_t>(Header->TrampolineOffset >> 32);
 			buffer[13] = 0x04;
 
-			return DetourCopyMemory(Header->CodeOffset, (sizeptr_t)&buffer, sizeof(buffer));
+			return DetourCopyMemory(Header->CodeOffset, reinterpret_cast<uintptr_t>(&buffer), sizeof(buffer));
 		}
 
 		bool DetourWriteRaxJump(JumpTrampolineHeader *Header)
@@ -263,12 +276,12 @@ namespace Detours
 			uint8_t buffer[12];
 			buffer[0] = 0x48;
 			buffer[1] = 0xB8;
-			*(uint64_t *)&buffer[2] = Header->TrampolineOffset;
+			*reinterpret_cast<uint64_t *>(&buffer[2]) = Header->TrampolineOffset;
 
 			buffer[10] = 0xFF;
 			buffer[11] = 0xE0;
 
-			return DetourCopyMemory(Header->CodeOffset, (sizeptr_t)&buffer, sizeof(buffer));
+			return DetourCopyMemory(Header->CodeOffset, reinterpret_cast<uintptr_t>(&buffer), sizeof(buffer));
 		}
 
 		bool DetourWriteRel32Jump(JumpTrampolineHeader *Header)
@@ -277,16 +290,34 @@ namespace Detours
 			//
 			// jmp offset64
 			//
-			int64_t displacement = (int64_t)(Header->TrampolineOffset - (Header->CodeOffset + 5));
+			const auto displacement = static_cast<int64_t>(Header->TrampolineOffset - (Header->CodeOffset + 5));
 
 			if (abs(displacement) >= INT_MAX)
 				return false;
 
 			uint8_t buffer[5];
 			buffer[0] = 0xE9;
-			*(int32_t *)&buffer[1] = (int32_t)displacement;
+			*reinterpret_cast<uint32_t *>(&buffer[1]) = static_cast<int32_t>(displacement);
 
-			return DetourCopyMemory(Header->CodeOffset, (sizeptr_t)&buffer, sizeof(buffer));
+			return DetourCopyMemory(Header->CodeOffset, reinterpret_cast<uintptr_t>(&buffer), sizeof(buffer));
+		}
+
+		bool DetourWriteRel32Call(JumpTrampolineHeader *Header)
+		{
+			// Same as DetourWriteRel32Jump but with a different opcode
+			//
+			// call offset64
+			//
+			const auto displacement = static_cast<int64_t>(Header->TrampolineOffset - (Header->CodeOffset + 5));
+
+			if (abs(displacement) >= INT_MAX)
+				return false;
+
+			uint8_t buffer[5];
+			buffer[0] = 0xE8;
+			*reinterpret_cast<uint32_t *>(&buffer[1]) = static_cast<int32_t>(displacement);
+
+			return DetourCopyMemory(Header->CodeOffset, reinterpret_cast<uintptr_t>(&buffer), sizeof(buffer));
 		}
 
 		uint32_t DetourGetHookLength(X64Option Options)
@@ -298,7 +329,7 @@ namespace Detours
 			case X64Option::USE_PUSH_RET:	size += PUSH_RET_LENGTH_64;	break;
 			case X64Option::USE_RAX_JUMP:	size += RAX_JUMP_64;		break;
 			case X64Option::USE_REL32_JUMP:	size += REL32_JUMP_64;		break;
-			default:						size = 0;					break;
+			case X64Option::USE_REL32_CALL:	size += REL32_CALL_64;		break;
 			}
 
 			return size;
